f614e23061012a4dfe4f3d20dfb701f7559f04b2
yacine180496 <benchehi@etud.insa-toulouse.fr>
2021-12-08 20:06:28 +0100

    Supervision Micro Service

---
+++ supervision/src/test/java/fr/insa/soa/supervision/SupervisionApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.insa.soa.supervision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupervisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
