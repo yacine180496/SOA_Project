35a57144e97e2e0b063b92bb9ba17606c0a673d1
yacine180496 <benchehi@etud.insa-toulouse.fr>
2021-12-08 19:34:08 +0100

    Micro Service Luminosity

---
+++ luminisoty/src/main/java/fr/insa/soa/luminisoty/LuminisotyApplication.java
@@ -0,0 +1,13 @@
+package fr.insa.soa.luminisoty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuminisotyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuminisotyApplication.class, args);
+	}
+
+}
---
+++ luminisoty/src/main/java/fr/insa/soa/luminisoty/controller/LuminosityController.java
@@ -0,0 +1,16 @@
+package fr.insa.soa.luminisoty.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+@RequestMapping("/luminosity") 
+public class LuminosityController {
+	
+	@GetMapping("/idValue")
+	public int ValueLuminosity() {
+		return (int)(Math.random()*100);
+	}
+}
